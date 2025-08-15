3f779980826d3ebf4f1b503799a81952f0332827
nahidn228 <nahidn228@gmail.com>
2025-08-15 23:36:28 +0600

    Refining Spacing and Alignment for Visual Consistency

--- src/App.tsx
+++ src/App.tsx
@@ -1,16 +1,11 @@
 import { Outlet } from "react-router";
 
 import CommonLayout from "./components/layout/CommonLayout";
-import Navbar from "./components/layout/Navbar";
-import Footer from "./components/layout/Footer";
 
 function App() {
   return (
     <CommonLayout>
-      <Navbar />
       <Outlet />
-
-      <Footer />
     </CommonLayout>
   );
 }
