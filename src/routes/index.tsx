ff8cd8ebb65babb9d0b9ff858bb15576b7894a41
nahidn228 <nahidn228@gmail.com>
2025-08-15 22:54:40 +0600

    create project structure

--- src/routes/index.tsx
+++ src/routes/index.tsx
@@ -1,28 +1,17 @@
 import App from "@/App";
-import AdminLayout from "@/components/layout/AdminLayout";
+
 import About from "@/pages/About";
-import Analytics from "@/pages/Analytics";
+
 import { createBrowserRouter } from "react-router";
 
 export const router = createBrowserRouter([
   {
-    // element: <App />,
     path: "/",
     Component: App,
     children: [
       {
         path: "/about",
         Component: About,
-      },
-    ],
-  },
-  {
-    Component: AdminLayout,
-    path: "admin",
-    children: [
-      {
-        path: "analytics",
-        Component: Analytics,
       },
     ],
   },
