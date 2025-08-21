d00970a48026cbadae71c0e8476fbc2f5c42dbc2
nahidn228 <nahidn228@gmail.com>
2025-08-21 22:32:49 +0600

    fix TypeScript error which is coming from React Hook Form

--- src/components/modules/admin/tour/AddTourModal.tsx
+++ src/components/modules/admin/tour/AddTourModal.tsx
@@ -19,62 +19,66 @@ import {
 import { Input } from "@/components/ui/input";
 import { useAddTourTypeMutation } from "@/redux/features/tour/tour.api";
 
-import { useForm } from "react-hook-form";
+import { useForm, type SubmitHandler } from "react-hook-form";
 import { toast } from "sonner";
 
+type TourTypeForm = {
+  name: string;
+};
+
 export function AddTourTypeModal() {
-  const form = useForm();
+  const form = useForm<TourTypeForm>();
   const [addTourType] = useAddTourTypeMutation();
-  const onSubmit = async (data) => {
+  const onSubmit: SubmitHandler<TourTypeForm> = async (data) => {
+    console.log(data);
     const res = await addTourType({ name: data.name }).unwrap();
 
     if (res.success) {
       toast.success("Tour Type Created");
+      form.reset();
     }
 
     console.log(res);
   };
   return (
     <Dialog>
-      <form>
-        <DialogTrigger asChild>
-          <Button>Add Tour Type</Button>
-        </DialogTrigger>
-        <DialogContent className="sm:max-w-[425px]">
-          <DialogHeader>
-            <DialogTitle>Add Tour Type</DialogTitle>
-          </DialogHeader>
-          <Form {...form}>
-            <form id="add-tour-type" onSubmit={form.handleSubmit(onSubmit)}>
-              <FormField
-                control={form.control}
-                name="name"
-                render={({ field }) => (
-                  <FormItem>
-                    <FormLabel>Tour Type</FormLabel>
-                    <FormControl>
-                      <Input
-                        placeholder="Tour Type Name"
-                        {...field}
-                        value={field.value || ""}
-                      />
-                    </FormControl>
-                    <FormMessage />
-                  </FormItem>
-                )}
-              />
-            </form>
-          </Form>
-          <DialogFooter>
-            <DialogClose asChild>
-              <Button variant="outline">Cancel</Button>
-            </DialogClose>
-            <Button form="add-tour-type" type="submit">
-              Save changes
-            </Button>
-          </DialogFooter>
-        </DialogContent>
-      </form>
+      <DialogTrigger asChild>
+        <Button>Add Tour Type</Button>
+      </DialogTrigger>
+      <DialogContent className="sm:max-w-[425px]">
+        <DialogHeader>
+          <DialogTitle>Add Tour Type</DialogTitle>
+        </DialogHeader>
+        <Form {...form}>
+          <form id="add-tour-type" onSubmit={form.handleSubmit(onSubmit)}>
+            <FormField
+              control={form.control}
+              name="name"
+              render={({ field }) => (
+                <FormItem>
+                  <FormLabel>Tour Type</FormLabel>
+                  <FormControl>
+                    <Input
+                      placeholder="Tour Type Name"
+                      {...field}
+                      value={field.value || ""}
+                    />
+                  </FormControl>
+                  <FormMessage />
+                </FormItem>
+              )}
+            />
+          </form>
+        </Form>
+        <DialogFooter>
+          <DialogClose asChild>
+            <Button variant="outline">Cancel</Button>
+          </DialogClose>
+          <Button form="add-tour-type" type="submit">
+            Save changes
+          </Button>
+        </DialogFooter>
+      </DialogContent>
     </Dialog>
   );
 }
